ca64cdd3e84e899708b879947db50092aff44915
JP <jessipearcy@gmail.com>
2020-11-29 14:54:50 -0600

    Publishing

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -7,16 +13,61 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./form.component.css'],
 })
 export class FormComponent {
-  public contact = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    email: new FormControl(''),
-    quantity: new FormControl(''),
-    message: new FormControl(''),
+  public existingCustomers = [
+    'existingCustomer@gmail.com',
+    'jessidevs@testEmail.com',
+  ];
+
+  public signup = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+      this.existingCustomer(),
+    ]),
+    quantity: new FormControl('', [
+      Validators.required,
+      Validators.min(2),
+      Validators.max(20),
+    ]),
+    message: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+    ]),
   });
 
+  get firstName() {
+    return this.signup.get('firstName');
+  }
+  get lastName() {
+    return this.signup.get('lastName');
+  }
+  get email() {
+    return this.signup.get('email');
+  }
+  get quantity() {
+    return this.signup.get('quantity');
+  }
+  get message() {
+    return this.signup.get('message');
+  }
+
+  existingCustomer(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } => {
+      if (!control.value) {
+        return null;
+      }
+
+      return !this.existingCustomers.includes(control.value)
+        ? null
+        : { existingCustomer: true };
+    };
+  }
+
   onSubmit() {
-    console.log(this.contact.value);
-    this.contact.reset();
+    console.log(this.signup.value);
+    console.log(this.quantity);
+    this.signup.reset();
   }
 }
